06d2da6fe0d08e4aae3c23e3d8e865928eb90c03
shino-lgtm <k@xmo.jp>
2025-05-25 18:51:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Advanced English Quiz</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Advanced English Conversation Quiz</h1>
+  <div id="quiz-container">
+    <div id="question-box">
+      <p id="question-text">Loading question...</p>
+    </div>
+    <div id="choices"></div>
+    <div id="feedback"></div>
+    <button id="next-button" style="display: none;">Next Question ▶</button>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
